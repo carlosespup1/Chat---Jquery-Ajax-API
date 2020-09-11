fba3233751fd096b3b0f77df4642b827ffb7d828
Carlos <obandocarlos604@gmail.com>
2020-09-11 10:33:00 -0600

    Primer versiona final, sin cambios finales

---
+++ Scripts/api-chat.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {                
+    setInterval(function(){ leerMensajes(); }, 1000);
+})
+
+var btnEnviar = document.getElementById('botonEnviar');
+
+function limpiarMensaje(){
+
+    var pantalla = document.getElementById('mensajeTXT');
+
+    pantalla.value = ' ';
+}
+
+btnEnviar.addEventListener('click', function(){
+
+    var usuario = document.getElementById('username').value;
+    var mensaje = document.getElementById('mensajeTXT').value;
+
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://ticosoftcr.website/chat/chat.php',
+        data: { "opcion": 2, "username": usuario, "mensaje": mensaje },
+        dataType: "json",
+        success: function (data) {
+            console.log("Envio");
+        }
+    });
+    
+    leerMensajes();
+    limpiarMensaje();
+});
+
+function leerMensajes() {
+    $.ajax({
+        type: 'Get',
+        url: 'http://ticosoftcr.website/chat/chat.php?opcion=1',
+        dataType: "json",
+        success: function (data) {
+            let HTML = "";
+            let linea = 0;
+
+            for (i = 0; i < data.length; i++){
+                HTML += "<div class = 'mensaje'>";
+                    HTML += "<b class = 'user'>" + data[i]["username"] + "</b>";
+                    HTML += "<br>";
+                    HTML += "<div class = 'caja-mensaje'>";
+                    HTML += "<i>" + data[i]["mensaje"] + "<br>" + data[i]["hora"];
+                    HTML += "</div>";
+                HTML += "</div>";
+            }
+
+            document.getElementById("mensajes").innerHTML = HTML;
+        },
+        error: function() {
+            alert("Esto fallo");
+        }
+    });
+}
